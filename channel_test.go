541b287535e4256c1d909ee205beab61966161f9
Nuruddin Ashr <uudashr@gmail.com>
2019-01-03 15:19:18 +0700

    Add benchmark for channel close vs no close

---
+++ channel_test.go
@@ -0,0 +1,37 @@
+package bench_test
+
+import (
+	"fmt"
+	"testing"
+)
+
+func BenchmarkChannel_Close(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		out := formatAsyncClose("Hello World %d", i)
+		<-out
+	}
+}
+
+func BenchmarkChannel_NoClose(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		out := formatAsyncNoClose("Hello World %d", i)
+		<-out
+	}
+}
+
+func formatAsyncClose(format string, a ...interface{}) <-chan string {
+	out := make(chan string)
+	go func() {
+		out <- fmt.Sprintf(format, a...)
+		close(out)
+	}()
+	return out
+}
+
+func formatAsyncNoClose(format string, a ...interface{}) <-chan string {
+	out := make(chan string)
+	go func() {
+		out <- fmt.Sprintf(format, a...)
+	}()
+	return out
+}
