1d6b17115d346a8a93f54e97b13967d584a6ac07
Nuruddin Ashr <uudashr@gmail.com>
2020-05-08 15:00:28 +0700

    Add benchmark test unsing func inside

--- defer_test.go
+++ defer_test.go
@@ -1,16 +1,31 @@
 package bench_test
 
-import "testing"
+import (
+	"fmt"
+	"io/ioutil"
+	"testing"
+)
 
 func doNoDefer(t *int) {
+	fmt.Fprintln(ioutil.Discard, "Main logic")
+
+	fmt.Fprintln(ioutil.Discard, "Closure")
+}
+
+func doNoDeferClosureFunc(t *int) {
+	fmt.Fprintln(ioutil.Discard, "Main logic")
+
 	func() {
-		*t++
+		fmt.Fprintln(ioutil.Discard, "Closure")
 	}()
 }
+
 func doDefer(t *int) {
 	defer func() {
 		*t++
+		fmt.Fprintln(ioutil.Discard, "Closure")
 	}()
+	fmt.Fprintln(ioutil.Discard, "Main logic")
 }
 
 func BenchmarkDeferYes(b *testing.B) {
@@ -19,9 +34,17 @@ func BenchmarkDeferYes(b *testing.B) {
 		doDefer(&t)
 	}
 }
+
 func BenchmarkDeferNo(b *testing.B) {
 	t := 0
 	for i := 0; i < b.N; i++ {
 		doNoDefer(&t)
+	}
+}
+
+func BenchmarkDeferNo_ClosureFunc(b *testing.B) {
+	t := 0
+	for i := 0; i < b.N; i++ {
+		doNoDeferClosureFunc(&t)
 	}
 }
