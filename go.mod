09f2bfb8e78f166ec2f303c21fd9efacbcfae90d
Nuruddin Ashr <uudashr@gmail.com>
2020-06-05 10:40:31 +0700

    Add UUID benchmark

--- go.mod
+++ go.mod
@@ -7,10 +7,13 @@ require (
 	github.com/cep21/circuit v3.0.0+incompatible
 	github.com/cep21/circuit/v3 v3.1.0 // indirect
 	github.com/go-kit/kit v0.10.0
+	github.com/google/uuid v1.1.1
 	github.com/gopherjs/gopherjs v0.0.0-20200217142428-fce0ec30dd00 // indirect
+	github.com/hashicorp/go-uuid v1.0.2
 	github.com/kr/text v0.2.0 // indirect
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/rubyist/circuitbreaker v2.2.1+incompatible
+	github.com/satori/go.uuid v1.2.0
 	github.com/sirupsen/logrus v1.6.0
 	github.com/smartystreets/assertions v1.1.0 // indirect
 	github.com/sony/gobreaker v0.4.1
---
+++ uuid_test.go
@@ -0,0 +1,63 @@
+package bench_test
+
+import (
+	"crypto/rand"
+	"fmt"
+	"io"
+	"testing"
+
+	hashicorpuuid "github.com/hashicorp/go-uuid"
+
+	satoriuuid "github.com/satori/go.uuid"
+
+	googleuuid "github.com/google/uuid"
+)
+
+func BenchmarkUUID_Google(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		s := googleuuid.New().String()
+		_ = s
+	}
+}
+
+func BenchmarkUUID_Hashicorp(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		s, err := hashicorpuuid.GenerateUUID()
+		if err != nil {
+			b.Fatal("Fail to generate UUID:", err)
+		}
+		_ = s
+	}
+}
+
+func BenchmarkUUID_Satori(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		s := satoriuuid.NewV4().String()
+		_ = s
+	}
+}
+
+func BenchmarkUUID_Custom(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		s, err := newUUID()
+		if err != nil {
+			b.Fatal("Fail to generate UUID:", err)
+		}
+		_ = s
+	}
+}
+
+// newUUID generates a random UUID according to RFC 4122
+// see: https://play.golang.org/p/4FkNSiUDMg
+func newUUID() (string, error) {
+	uuid := make([]byte, 16)
+	n, err := io.ReadFull(rand.Reader, uuid)
+	if n != len(uuid) || err != nil {
+		return "", err
+	}
+	// variant bits; see section 4.1.1
+	uuid[8] = uuid[8]&^0xc0 | 0x80
+	// version 4 (pseudo-random); see section 4.1.3
+	uuid[6] = uuid[6]&^0xf0 | 0x40
+	return fmt.Sprintf("%x-%x-%x-%x-%x", uuid[0:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:]), nil
+}
