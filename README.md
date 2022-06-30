2fabbd46e9bf4edd6582b0b12f64eab02b8b0989
Nuruddin Ashr <nuruddin.ashr@grabtaxi.com>
2022-06-30 17:18:33 +0700

    Add string conversion from int64

--- README.md
+++ README.md
@@ -3,6 +3,12 @@
 Collection of go benchmarks.
 
 ## Usage
+Run all benchmark
 ```
 go test -benchmem -run="^$" -bench=.
+```
+
+Run specific benchmark
+```
+go test -benchmem -run="^$" -bench="^BenchmarkStringConvert_"
 ```
--- strconv_test.go
+++ strconv_test.go
@@ -2,6 +2,7 @@ package bench_test
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 	"testing"
 )
@@ -13,9 +14,23 @@ func BenchmarkStringConvert_Itoa(b *testing.B) {
 	}
 }
 
-func BenchmarkStringConvert_Sprintf(b *testing.B) {
+func BenchmarkStringConvert_Sprint(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		_ = fmt.Sprint(i)
+	}
+}
+
+func BenchmarkStringConvert_Int64Itoa(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = strconv.FormatUint(uint64(math.MaxInt64)+1, 10)
+	}
+}
+
+func BenchmarkStringConvert_Int64Sprint(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = fmt.Sprint(uint64(math.MaxInt64) + 1)
 	}
 }
