b78fd40adcccf799c7be4b68e3bb36b72606caf7
Nuruddin Ashr <nuruddin.ashr@grabtaxi.com>
2022-06-30 17:22:08 +0700

    Fix the deprecation path of go-kit package

--- log_test.go
+++ log_test.go
@@ -9,8 +9,8 @@ import (
 
 	"github.com/sirupsen/logrus"
 
-	kitlog "github.com/go-kit/kit/log"
-	"github.com/go-kit/kit/log/level"
+	kitlog "github.com/go-kit/log"
+	"github.com/go-kit/log/level"
 )
 
 func BenchmarkLog_LogrusDebugLevel(b *testing.B) {
