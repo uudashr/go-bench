348bd0fb9b793cc3b71b5059c4b03bf4628c37c4
Nuruddin Ashr <uudashr@gmail.com>
2024-07-12 17:31:05 +0700

    Improved test performance using MapFS

--- reader_test.go
+++ reader_test.go
@@ -2,16 +2,21 @@ package bench_test
 
 import (
 	"bytes"
+	_ "embed"
 	"io"
+	"io/fs"
 	"os"
 	"strings"
 	"testing"
+	"testing/fstest"
 )
 
-func BenchmarkReadReader_readAllJSONString(b *testing.B) {
+func BenchmarkReadReader_DirFS_readAllJSONString(b *testing.B) {
+	afs := dirFS()
+
 	for i := 0; i < b.N; i++ {
 		b.StopTimer()
-		f, err := os.Open("testdata/json/error.json")
+		f, err := afs.Open("error.json")
 		if err != nil {
 			b.Fatal(err)
 		}
@@ -31,10 +36,12 @@ func BenchmarkReadReader_readAllJSONString(b *testing.B) {
 	}
 }
 
-func BenchmarkReadReader_stringsBuilderJSON(b *testing.B) {
+func BenchmarkReadReader_DirFS_stringsBuilderJSON(b *testing.B) {
+	afs := dirFS()
+
 	for i := 0; i < b.N; i++ {
 		b.StopTimer()
-		f, err := os.Open("testdata/json/error.json")
+		f, err := afs.Open("error.json")
 		if err != nil {
 			b.Fatal(err)
 		}
@@ -55,10 +62,12 @@ func BenchmarkReadReader_stringsBuilderJSON(b *testing.B) {
 	}
 }
 
-func BenchmarkReadReader_bytesBufferJSONString(b *testing.B) {
+func BenchmarkReadReader_DirFS_bytesBufferJSONString(b *testing.B) {
+	afs := dirFS()
+
 	for i := 0; i < b.N; i++ {
 		b.StopTimer()
-		f, err := os.Open("testdata/json/error.json")
+		f, err := afs.Open("error.json")
 		if err != nil {
 			b.Fatal(err)
 		}
@@ -77,4 +86,96 @@ func BenchmarkReadReader_bytesBufferJSONString(b *testing.B) {
 		f.Close()
 		b.StartTimer()
 	}
+}
+
+func BenchmarkReadReader_MapFS_readAllJSONString(b *testing.B) {
+	afs := mapFS()
+
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		f, err := afs.Open("error.json")
+		if err != nil {
+			b.Fatal(err)
+		}
+		b.StartTimer()
+
+		out, err := io.ReadAll(f)
+		if err != nil {
+			b.Fatal(err)
+		}
+
+		s := string(out)
+		_ = s
+
+		b.StopTimer()
+		f.Close()
+		b.StartTimer()
+	}
+}
+
+func BenchmarkReadReader_MapFS_stringsBuilderJSON(b *testing.B) {
+	afs := mapFS()
+
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		f, err := afs.Open("error.json")
+		if err != nil {
+			b.Fatal(err)
+		}
+		b.StartTimer()
+
+		var sb strings.Builder
+		_, err = io.Copy(&sb, f)
+		if err != nil {
+			b.Fatal(err)
+		}
+
+		s := sb.String()
+		_ = s
+
+		b.StopTimer()
+		f.Close()
+		b.StartTimer()
+	}
+}
+
+func BenchmarkReadReader_MapFS_bytesBufferJSONString(b *testing.B) {
+	afs := mapFS()
+
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		f, err := afs.Open("error.json")
+		if err != nil {
+			b.Fatal(err)
+		}
+		b.StartTimer()
+
+		var buf bytes.Buffer
+		_, err = buf.ReadFrom(f)
+		if err != nil {
+			b.Fatal(err)
+		}
+
+		s := buf.String()
+		_ = s
+
+		b.StopTimer()
+		f.Close()
+		b.StartTimer()
+	}
+}
+
+//go:embed testdata/json/error.json
+var jsonString string
+
+func mapFS() fstest.MapFS {
+	return fstest.MapFS{
+		"error.json": {
+			Data: []byte(jsonString),
+		},
+	}
+}
+
+func dirFS() fs.FS {
+	return os.DirFS("testdata/json")
 }
