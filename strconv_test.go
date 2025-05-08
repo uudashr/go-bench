667c9fc8d7dd34a354b70b9395ffe61b521e6ecf
Nuruddin Ashr <uudashr@gmail.com>
2025-05-08 11:20:20 +0700

    Update strconv

--- strconv_test.go
+++ strconv_test.go
@@ -2,35 +2,58 @@ package bench_test
 
 import (
 	"fmt"
-	"math"
 	"strconv"
 	"testing"
 )
 
 func BenchmarkStringConvert_Itoa(b *testing.B) {
-
 	for i := 0; b.Loop(); i++ {
 		_ = strconv.Itoa(i)
 	}
 }
 
-func BenchmarkStringConvert_Sprint(b *testing.B) {
-
+func BenchmarkStringConvert_SprintInt(b *testing.B) {
 	for i := 0; b.Loop(); i++ {
 		_ = fmt.Sprint(i)
 	}
 }
 
-func BenchmarkStringConvert_Int64Itoa(b *testing.B) {
-
-	for b.Loop() {
-		_ = strconv.FormatUint(uint64(math.MaxInt64)+1, 10)
+func BenchmarkStringConvert_SprintInt64(b *testing.B) {
+	for i := int64(0); b.Loop(); i++ {
+		_ = fmt.Sprint(i)
 	}
 }
 
-func BenchmarkStringConvert_Int64Sprint(b *testing.B) {
-
-	for b.Loop() {
-		_ = fmt.Sprint(uint64(math.MaxInt64) + 1)
+func BenchmarkStringConvert_SprintUint64(b *testing.B) {
+	for i := uint64(0); b.Loop(); i++ {
+		_ = fmt.Sprint(i)
 	}
+}
+
+func BenchmarkStringConvert_FormatInt_non64(b *testing.B) {
+	for i := 0; b.Loop(); i++ {
+		_ = strconv.FormatInt(int64(i), 10)
+	}
+}
+
+func BenchmarkStringConvert_FormatInt_indirection(b *testing.B) {
+	for i := 0; b.Loop(); i++ {
+		_ = formatInt(i)
+	}
+}
+
+func BenchmarkStringConvert_FormatInt(b *testing.B) {
+	for i := int64(0); b.Loop(); i++ {
+		_ = strconv.FormatInt(i, 10)
+	}
+}
+
+func BenchmarkStringConvert_FormatUint(b *testing.B) {
+	for i := uint64(0); b.Loop(); i++ {
+		_ = strconv.FormatUint(i, 10)
+	}
+}
+
+func formatInt(i int) string {
+	return strconv.FormatInt(int64(i), 10)
 }
